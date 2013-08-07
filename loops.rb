6766e3ce3e6810540b04e64246e57b7a844292ab
Jonathan Wilson <mrjonwilson@gmail.com>
2013-08-06 22:57:00 -0500

    Created a program that allows users to create a box with even sides.

--- loops.rb
+++ loops.rb
@@ -6,6 +6,7 @@
 # Some statement in the while loop that will modify the boolean expression.
 #   some way of ending the loop
 
+=begin
 # Count from 1 through 10
 count = 100
 while count >= 0
@@ -14,3 +15,28 @@ while count >= 0
   end
   count -= 1
 end
+=end
+#########################
+=begin
+8 sided box
+********   Same as last
+*      *
+*      *
+*      *   6 duplicated lines
+*      *
+*      *
+*      *
+********
+
+=end
+
+print "Enter the size of the box: "
+size = gets.to_i
+
+puts "*" * size
+count = 0
+while count < size - 2
+  puts "*" + " " * (size - 2) + "*"
+  count += 1
+end
+puts "*" * size
